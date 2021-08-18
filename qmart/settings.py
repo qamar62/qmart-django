e6605abf29799882605ba33f33831b211a2ea100
QAM <qam600@gmail.com>
2021-08-19 01:23:13 +0500

    cart

--- qmart/settings.py
+++ qmart/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'shop',
     'category',
     'accounts',
+    'carts',
     
     
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,9 @@
 
+from carts.models import CartItem
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.views import _cart_id
 
 # Create your views here.
 def home(request):
@@ -33,9 +35,11 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart =  CartItem.objects.filter(cart__cart_id= _cart_id(request)).exists()
+        
 
     except Exception as e:
         raise e
 
-    context = {'single_product': single_product}
+    context = {'single_product': single_product, 'in_cart': in_cart}
     return render(request, 'shop\product_detail.html', context)
